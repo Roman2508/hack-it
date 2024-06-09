e5473f6784616156bc7e19e72a6ef08ebdc43381
Roman Ptashnyk <100701048+Roman2508@users.noreply.github.com>
2024-06-09 13:01:37 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,180 +1,195 @@
 import pygame
 import random
-import pygame as pg
 
-run = True
-window_x = 600
-window_y = 700
-window = pygame.display.set_mode((window_x, window_y))
-background_level1 = pg.image.load("background.png")
-diamond1_picture = pg.image.load("8.png")
-diamond2_picture = pg.image.load("9.png")
-diamond3_picture = pg.image.load("11.png")
-hero_direction = 'STOP'
-count_before_dimond = 0
-pygame.font.init()
-font = pygame.font.SysFont('Arial', 26)
-white = (255, 255, 255)
-viole_dark = (47, 14, 51)
-text_x = 10
-text_y = 10
+pygame.init()
 
-def draw_level1(window, picture):
-    window.blit(picture, (0, 0))
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
-class wizard:
-    x = 10
-    y = 500
-    width = 138
-    height = 150
-    speed = 4
-    main_picture = pg.image.load("1_IDLE_000.png")
-    left_picture = pg.image.load("3_RUN_000_l.png")
-    right_picture = pg.image.load("3_RUN_000.png")
-    def stand(self, window):
-        window.blit(self.main_picture, (self.x, self.y))
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
 
-    def move_left(self, window):
-        if self.x - self.speed >= 0:
-            self.x -= self.speed
-        else:
-            self.x = 0
-        window.blit(self.left_picture, (self.x, self.y))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('2D Platformer')
 
-    def move_right(self, window, width):
-        if self.x + self.speed <= width - self.width:
-            self.x += self.speed
-        else:
-            self.x = width - self.width
-        self.x += self.speed
-        window.blit(self.right_picture, (self.x, self.y))
-
-    def wizard_position(self):
-        return (self.x, self.y)
-    def size(self):
-        return (self.width, self.height)
-class super_wizard(wizard):
-    width = 150
-    height = 175
-    main_picture = pg.image.load("1_IDLE_000_i.png")
-    left_picture = pg.image.load("3_RUN_000_il.png")
-    right_picture = pg.image.load("3_RUN_000_i.png")
-    jump_picture = pg.image.load("4_JUMP_003.png")
-
-    def jump(self, window):
-        window.blit(self.jump_picture, (self.x, self.y-70))
+original_background = pygame.image.load('background.png').convert()
+background = pygame.transform.scale(original_background, (800, 600))
 
 
-class diamond():
-    x = 0
-    y = 0
-    speed = 0
-    picture = 0
-
-    def __init__(self, picture):
-        self.x = random.randint(0, 553)
-        self.speed = random.randint(1, 4)
-        self.picture = picture
-
-    def show(self, window):
-        window.blit(self.picture, (self.x, self.y))
-
-    def fall(self):
-        self.y += self.speed
-    def diamond_position(self):
-        return (self.x, self.y)
-
-class diamonds:
-    diamonds_pictures = [pg.image.load("11.png"), pg.image.load("8.png"), pg.image.load("9.png")]
-    diamonds_list = []
-
+class Player(pygame.sprite.Sprite):
     def __init__(self):
-        pass
+        super().__init__()
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.x = 100
+        self.rect.y = 500
+        self.change_x = 0
+        self.change_y = 0
+        self.jump_count = 10
+
+    def update(self):
+        self.calc_grav()
+        self.rect.x += self.change_x
+
+        block_hit_list = pygame.sprite.spritecollide(self, platforms, False)
+        for block in block_hit_list:
+            if self.change_x > 0:
+                self.rect.right = block.rect.left
+            elif self.change_x < 0:
+                self.rect.left = block.rect.right
+
+        self.rect.y += self.change_y
+
+        block_hit_list = pygame.sprite.spritecollide(self, platforms, False)
+        for block in block_hit_list:
+            if self.change_y > 0:
+                self.rect.bottom = block.rect.top
+            elif self.change_y < 0:
+                self.rect.top = block.rect.bottom
+
+            self.change_y = 0
+
+    def calc_grav(self):
+        if self.change_y == 0:
+            self.change_y = 1
+        else:
+            self.change_y += .35
+
+        if self.rect.y >= SCREEN_HEIGHT - self.rect.height and self.change_y >= 0:
+            self.change_y = 0
+            self.rect.y = SCREEN_HEIGHT - self.rect.height
+
+    def jump(self):
+        self.rect.y += 2
+        platform_hit_list = pygame.sprite.spritecollide(self, platforms, False)
+        self.rect.y -= 2
+
+        if len(platform_hit_list) > 0 or self.rect.bottom >= SCREEN_HEIGHT:
+            self.change_y = -10
+
+    def go_left(self):
+        self.change_x = -6
+
+    def go_right(self):
+        self.change_x = 6
+
+    def stop(self):
+        self.change_x = 0
+
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, width, height, x, y):
+        super().__init__()
+        self.image = pygame.Surface((width, height))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+class Collectible(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((20, 20))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.spawn_time = pygame.time.get_ticks()
+        self.reposition()
+
+    def update(self):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.spawn_time > 5000:
+            self.reposition()
+
+    def reposition(self):
+        self.rect.y = random.choice([180, 280, 380, 480])
+        if self.rect.y == 180 or self.rect.y == 380:
+            self.rect.x = random.randint(400, 580)
+        if self.rect.y == 280 or self.rect.y == 480:
+            self.rect.x = random.randint(100, 280)
+        self.spawn_time = pygame.time.get_ticks()
+
+    def check_collected(self, player):
+        if self.rect.colliderect(player.rect):
+            self.reposition()
+            return True
+        return False
+
+
+class Collectibles:
+    def __init__(self):
+        self.collectibles_list = pygame.sprite.Group()
+        for _ in range(4):
+            self.add()
 
     def add(self):
-        self.diamonds_list.append(diamond(self.diamonds_pictures[random.randint(0,2)]))
+        collectible = Collectible()
+        self.collectibles_list.add(collectible)
+
+    def update(self, player):
+        for collectible in self.collectibles_list:
+            collectible.update()
+            if collectible.check_collected(player):
+                collectible.reposition()
+                return True
+        return False
 
     def draw(self, window):
-        for element in self.diamonds_list:
-            element.show(window)
-
-    def fall(self):
-        for element in self.diamonds_list:
-            element.fall()
-
-    def delete(self, x, y, width, height):
-        count = 0
-        lost = 0
-        cach = 0
-        for element in self.diamonds_list:
-            position = element.diamond_position()
-            d_x = position[0]
-            d_y = position[1]+47
-            if d_x > x and d_x < x + width and d_y > y and d_y < y + height:
-                del self.diamonds_list[count]
-                cach += 1
-            elif position[1] > 700:
-                del self.diamonds_list[count]
-                lost += 1
-        count += 1
-        return (cach, lost)
+        self.collectibles_list.draw(window)
 
 
-hero = wizard()
-super_hero = super_wizard()
-diamonds_IN_game = diamonds()
-diamonds_IN_game.add()
-catch = 0
-lost = 0
-run_level = True
+player = Player()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
 
-while run:
-    while run_level:
-        if count_before_dimond == 200:
-            diamonds_IN_game.add()
-            count_before_dimond = 0
-        draw_level1(window, background_level1)
-        massage = "Score: " + str(catch) + " / " + str(lost)
-        text = font.render(massage, True, white, viole_dark)
-        window.blit(text, (text_x, text_y))
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                run_level = False
-                break
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-                hero_direction = 'LEFT'
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-                hero_direction = 'RIGHT'
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                hero_direction = 'UP'
-            elif event.type == pygame.KEYUP:
-                hero_direction = 'STOP'
-        if hero_direction == 'LEFT':
-            super_hero.move_left(window)
-        elif hero_direction == 'RIGHT':
-            super_hero.move_right(window, window_x)
-        elif hero_direction == 'UP':
-            super_hero.jump(window)
-        else:
-            super_hero.stand(window)
-        diamonds_IN_game.draw(window)
-        pg.display.update()
-        count_before_dimond += 1
-        wizard_position = super_hero.wizard_position()
-        wizard_size = super_hero.size()
-        result = diamonds_IN_game.delete(wizard_position[0], wizard_position[1], wizard_size[0], wizard_size[1])
-        catch += result[0]
-        lost += result[1]
-        diamonds_IN_game.fall()
-        if lost == 6:
-            run_level = False
-    window.blit(background_level1, (0, 0))
-    font = pygame.font.SysFont('Arial', 46)
-    massage = "Game over! Your Score: " + str(catch)
-    text = font.render(massage, True, white, viole_dark)
-    window.blit(text, (text_x, text_y))
-    pg.display.update()
+platforms = pygame.sprite.Group()
+platform_list = [
+    Platform(200, 20, 100, 500),
+    Platform(200, 20, 400, 400),
+    Platform(200, 20, 100, 300),
+    Platform(200, 20, 400, 200),
+]
+
+for platform in platform_list:
+    platforms.add(platform)
+    all_sprites.add(platform)
+
+collectibles = Collectibles()
+
+running = True
+clock = pygame.time.Clock()
+
+
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            running = False
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.go_left()
+            elif event.key == pygame.K_RIGHT:
+                player.go_right()
+            elif event.key == pygame.K_SPACE or event.key == pygame.K_UP:
+                player.jump()
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player.stop()
+
+    all_sprites.update()
+    collectibles.update(player)
+
+    screen.blit(background, (0, 0))
+    all_sprites.draw(screen)
+    collectibles.draw(screen)
+
+    pygame.display.update()
+
+    clock.tick(60)
+
+pygame.quit()
