79cd5f02fb77dcd60b1716843cf2da34afcbce82
Roman Ptashnyk <100701048+Roman2508@users.noreply.github.com>
2024-06-09 10:09:38 +0300

    Add files via upload

---
+++ main — копия.py
@@ -0,0 +1,140 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('2D Platformer')
+
+original_background = pygame.image.load('background.png').convert()
+
+background = pygame.transform.scale(original_background, (800, 600))
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.x = 100
+        self.rect.y = 500
+        self.change_x = 0
+        self.change_y = 0
+        self.jump_count = 10
+
+    def update(self):
+        self.calc_grav()
+        self.rect.x += self.change_x
+
+        block_hit_list = pygame.sprite.spritecollide(self, platforms, False)
+        for block in block_hit_list:
+            if self.change_x > 0:
+                self.rect.right = block.rect.left
+            elif self.change_x < 0:
+                self.rect.left = block.rect.right
+
+        self.rect.y += self.change_y
+
+        block_hit_list = pygame.sprite.spritecollide(self, platforms, False)
+        for block in block_hit_list:
+            if self.change_y > 0:
+                self.rect.bottom = block.rect.top
+            elif self.change_y < 0:
+                self.rect.top = block.rect.bottom
+
+            self.change_y = 0
+
+    def calc_grav(self):
+        if self.change_y == 0:
+            self.change_y = 1
+        else:
+            self.change_y += .35
+
+        if self.rect.y >= SCREEN_HEIGHT - self.rect.height and self.change_y >= 0:
+            self.change_y = 0
+            self.rect.y = SCREEN_HEIGHT - self.rect.height
+
+    def jump(self):
+        self.rect.y += 2
+        platform_hit_list = pygame.sprite.spritecollide(self, platforms, False)
+        self.rect.y -= 2
+
+        if len(platform_hit_list) > 0 or self.rect.bottom >= SCREEN_HEIGHT:
+            self.change_y = -10
+
+    def go_left(self):
+        self.change_x = -6
+
+    def go_right(self):
+        self.change_x = 6
+
+    def stop(self):
+        self.change_x = 0
+
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, width, height, x, y):
+        super().__init__()
+        self.image = pygame.Surface((width, height))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+player = Player()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+platforms = pygame.sprite.Group()
+platform_list = [
+    Platform(200, 20, 100, 500),
+    Platform(200, 20, 400, 400),
+    Platform(200, 20, 100, 300),
+    Platform(200, 20, 400, 200),
+]
+
+for platform in platform_list:
+    platforms.add(platform)
+    all_sprites.add(platform)
+
+
+running = True
+clock = pygame.time.Clock()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.go_left()
+            elif event.key == pygame.K_RIGHT:
+                player.go_right()
+            elif event.key == pygame.K_SPACE or event.key == pygame.K_UP:
+                player.jump()
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player.stop()
+
+    all_sprites.update()
+
+    screen.blit(background, (0, 0))
+    all_sprites.draw(screen)
+
+    pygame.display.update()
+
+    clock.tick(60)
+
+pygame.quit()
